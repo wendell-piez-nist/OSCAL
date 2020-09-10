38b8f10d9159f462c082fc234388780e1a95cd12
Wendell Piez <wendell.piez@nist.gov>
2020-09-10 17:29:25 -0400

    Reworking run-xslt transform to apply XSLT to any file named on the CL

--- build/ci-cd/typescript-xslt/src/run-xslt.ts
+++ build/ci-cd/typescript-xslt/src/run-xslt.ts
@@ -1,9 +1,11 @@
 ﻿#!/usr/bin/env node
 
-import * as SaxonJS from "../node_modules/saxon-js/SaxonJS2N.js";
+// import * as SaxonJS from "../node_modules/saxon-js/SaxonJS2N.js";
+const SaxonJS = require('SaxonJS');
+
+const workingDir: string = process.cwd()
 
 /*nameIn is a file named on the cl */
-const process = require('process')
 const nameIn: string = process.argv[2]
 
 /*const sysPath: string = 'file:///C:/Users/wap1/source/repos/NodejsTestConsole/src'*/
@@ -13,11 +15,9 @@ const nameIn: string = process.argv[2]
  * xslt3 - t - xsl: hello-world.xsl -export: hello.sef.json - nogo
 */
 
-let sourceXML: string = nameIn
-/*let helloString: string = '<hello>Baby doll! 1001</hello>'*/
-let xsltFile: string = 'hello-world.xsl'
+let sourceXML: string = `${workingDir}/{nameIn}`
+let xsltFile:  string = 'hello-world.xsl'
 let sefFile: string = 'hello.sef.json'
-/*let resultFile: string = [sysPath, 'result.xml'].join('/')*/
 
 /*API docs: http://www.saxonica.com/saxon-js/documentation/index.html#!api/transform */
 
@@ -40,6 +40,3 @@ SaxonJS.transform({
     response.write(output.principalResult);
     response.end();
 });  */
-
-/*let message: string = "Hello World"
-console.log(message)*/
